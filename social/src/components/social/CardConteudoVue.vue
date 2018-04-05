f25fa069d836be09eca2ff99c13a0f27e3eb3241
Samuel Terra <samuelterra22@hotmail.com>
2018-04-05 14:35:50 -0300

    Otimiza componentes do card de conteudo

--- social/src/components/social/CardConteudoVue.vue
+++ social/src/components/social/CardConteudoVue.vue
@@ -1,25 +1,27 @@
 <template>
-  <div class="card">
-    <div class="card-content">
-      <div class="row valign-wrapper">
-        <grid-vue tamanho="1">
-          <img :src="perfil" :alt="nome" class="circle responsive-img">
-        </grid-vue>
-        <grid-vue tamanho="11">
+  <div class="row">
+    <div class="card">
+      <div class="card-content">
+        <div class="row valign-wrapper">
+          <grid-vue tamanho="1">
+            <img :src="perfil" :alt="nome" class="circle responsive-img">
+          </grid-vue>
+          <grid-vue tamanho="11">
           <span class="black-text">
             <strong>{{nome}}</strong> - <small>{{data}}</small>
           </span>
-        </grid-vue>
+          </grid-vue>
+        </div>
+
+        <slot/>
+
       </div>
-
-      <slot />
-
-    </div>
-    <div class="card-action">
-      <p>
-        <i class="material-icons">favorite_border</i>
-        <i class="material-icons">insert_comment</i>
-      </p>
+      <div class="card-action">
+        <p>
+          <i class="material-icons">favorite_border</i>
+          <i class="material-icons">insert_comment</i>
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -30,7 +32,7 @@
   export default {
     name: 'CardConteudoVue',
     props: ['perfil', 'nome', 'data'],
-    components:{
+    components: {
       GridVue
     },
     data () {
--- social/src/pages/home/Home.vue
+++ social/src/pages/home/Home.vue
@@ -1,22 +1,38 @@
 <template>
-  <card-conteudo-vue
-    perfil="http://materializecss.com/images/yuna.jpg"
-    nome="Maria Silva"
-    data="13/01/18 13:30">
+
+  <span>
+
+     <div class="row">
+        <grid-vue class="input-field" tamanho="12">
+          <textarea id="textarea1" class="materialize-textarea"></textarea>
+          <label for="textarea1">O que está acontecendo?</label>
+        </grid-vue>
+       <p>
+         <grid-vue class="btn waves-effect waves-light" tamanho="2 offset-s10">Publicar</grid-vue>
+       </p>
+     </div>
+
+    <card-conteudo-vue
+      perfil="http://materializecss.com/images/yuna.jpg"
+      nome="Maria Silva"
+      data="13/01/18 13:30">
 
     <card-detalhe-vue
-    imagem="http://materializecss.com/images/sample-1.jpg"
-    titulo="Card Title"
-    texto="I am a very simple card. I am good at containing small bits of information.
+      imagem="http://materializecss.com/images/sample-1.jpg"
+      titulo="Card Title"
+      texto="I am a very simple card. I am good at containing small bits of information.
         I am convenient because I require little markup to use effectively."
     />
 
   </card-conteudo-vue>
+  </span>
+
 </template>
 
 <script>
   import CardConteudoVue from '@/components/social/CardConteudoVue'
   import CardDetalheVue from '@/components/social/CardDetalheVue'
+  import GridVue from '@/components/layouts/GridVue'
 
   export default {
     name: 'Home',
@@ -27,7 +43,8 @@
     },
     components: {
       CardConteudoVue,
-      CardDetalheVue
+      CardDetalheVue,
+      GridVue
     }
   }
 </script>
