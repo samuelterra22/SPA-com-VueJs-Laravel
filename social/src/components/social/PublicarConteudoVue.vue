f580c463cbe72d144bf0e2b6fa701193387bc7de
Samuel Terra <samuelterra22@hotmail.com>
2018-04-05 14:43:54 -0300

    Separa componente de publicar conteudo

---
+++ social/src/components/social/PublicarConteudoVue.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="row">
+    <grid-vue class="input-field" tamanho="12">
+      <textarea v-model="conteudo" id="conteudoID" class="materialize-textarea"></textarea>
+      <label for="conteudoID">O que está acontecendo?</label>
+    </grid-vue>
+    <p>
+      <grid-vue v-if="conteudo" class="btn waves-effect waves-light" tamanho="2 offset-s10">Publicar</grid-vue>
+    </p>
+  </div>
+</template>
+
+<script>
+  import GridVue from '@/components/layouts/GridVue'
+
+  export default {
+    name: 'PublicarConteudoVue',
+    props: [],
+    components: {
+      GridVue
+    },
+    data () {
+      return {
+        conteudo: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- social/src/pages/home/Home.vue
+++ social/src/pages/home/Home.vue
@@ -2,29 +2,21 @@
 
   <span>
 
-     <div class="row">
-        <grid-vue class="input-field" tamanho="12">
-          <textarea id="textarea1" class="materialize-textarea"></textarea>
-          <label for="textarea1">O que está acontecendo?</label>
-        </grid-vue>
-       <p>
-         <grid-vue class="btn waves-effect waves-light" tamanho="2 offset-s10">Publicar</grid-vue>
-       </p>
-     </div>
+    <publicar-conteudo-vue/>
 
-    <card-conteudo-vue
-      perfil="http://materializecss.com/images/yuna.jpg"
-      nome="Maria Silva"
-      data="13/01/18 13:30">
+      <card-conteudo-vue
+        perfil="http://materializecss.com/images/yuna.jpg"
+        nome="Maria Silva"
+        data="13/01/18 13:30">
 
-    <card-detalhe-vue
-      imagem="http://materializecss.com/images/sample-1.jpg"
-      titulo="Card Title"
-      texto="I am a very simple card. I am good at containing small bits of information.
-        I am convenient because I require little markup to use effectively."
-    />
+      <card-detalhe-vue
+        imagem="http://materializecss.com/images/sample-1.jpg"
+        titulo="Card Title"
+        texto="I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively."
+      />
 
-  </card-conteudo-vue>
+    </card-conteudo-vue>
   </span>
 
 </template>
@@ -32,7 +24,7 @@
 <script>
   import CardConteudoVue from '@/components/social/CardConteudoVue'
   import CardDetalheVue from '@/components/social/CardDetalheVue'
-  import GridVue from '@/components/layouts/GridVue'
+  import PublicarConteudoVue from '@/components/social/PublicarConteudoVue'
 
   export default {
     name: 'Home',
@@ -44,7 +36,7 @@
     components: {
       CardConteudoVue,
       CardDetalheVue,
-      GridVue
+      PublicarConteudoVue
     }
   }
 </script>
