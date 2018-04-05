edd0e1b3d002b867323ccdfd1bf46bccca8afcb8
Samuel Terra <samuelterra22@hotmail.com>
2018-04-05 11:50:40 -0300

    Commit inicial

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="">
+    <h2>Social</h2>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
