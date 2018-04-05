239d3ff5af97ac9358b2d15c6822865e4e0b06f4
Samuel Terra <samuelterra22@hotmail.com>
2018-04-05 19:03:52 -0300

    Adiciona validação de ussuários

--- webservice/routes/api.php
+++ webservice/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,17 @@ use Illuminate\Http\Request;
 
 Route::post('/cadastro', function (Request $request) {
     $data = $request->all();
+
+    $validacao = Validator::make($data, [
+        'name'     => 'required|string|max:255',
+        'email'    => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:6|confirmed',
+    ]);
+
+    if($validacao->fails()){
+        return $validacao->errors();
+    }
+
     $user = User::create([
         'name'     => $data['name'],
         'email'    => $data['email'],
