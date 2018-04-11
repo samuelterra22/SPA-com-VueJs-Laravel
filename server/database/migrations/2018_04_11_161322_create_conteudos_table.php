4ccd55f33190aa4f424ffbbd1ac09986c1f34994
Samuel Terra <samuelterra22@hotmail.com>
2018-04-11 13:16:51 -0300

    Modela o banco de dados

---
+++ server/database/migrations/2018_04_11_161322_create_conteudos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConteudosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conteudos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conteudos');
+    }
+}
